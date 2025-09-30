28422e012b540f852606896c8d77f8d08222247f
machineke <markarapsoi72@gmail.com>
2025-09-30 13:04:32 +0300

    improve component

--- src/components/AllProductsView/AllProductsView.tsx
+++ src/components/AllProductsView/AllProductsView.tsx
@@ -72,7 +72,7 @@ export const AllProductsView: React.FC<AllProductsViewProps> = ({
       <PageHeader>
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
-            <span className="font-bold text-xl">Marketplace</span>
+            {/* <span className="font-bold text-xl">Marketplace</span> */}
             <div className="flex items-center space-x-4">
               {enableSearch && (
                 <Input
